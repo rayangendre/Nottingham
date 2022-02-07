f64319b305563c03306da37afe80479cfad905ea
rayangendre <rayan.gendre@gmail.com>
2022-02-07 10:48:51 -0800

    created the router

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Nottingham!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>The leader in stock portfolio</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          Here to help your portfolio grow!
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
