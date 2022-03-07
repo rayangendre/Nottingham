c2a3aacb579f9f9c550b3a0fa518deadef70eaf0
c1jb <cbohley@calpoly.edu>
2022-03-07 11:07:41 -0800

    adding some polygon changes

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -3,8 +3,11 @@ import { Routes, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button"
 import './App.css';
-import '../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries} from 'react-vis';
+//import '../node_modules/react-vis/dist/style.css';
+//import {XYPlot, LineSeries} from 'react-vis';
+import { polygonClient, restClient, websocketClient } from "@polygon.io/client-js";
+const rest = restClient("nKHbbu_36PUwzwk9Ed90ViqDpxk4GFHH");
+const stocksWS = websocketClient("nKHbbu_36PUwzwk9Ed90ViqDpxk4GFHH").stocks();
 
 function App() {
   return (
@@ -115,12 +118,33 @@ function LogIn() {
 }
 
 function StockCheck(){
+  let myvalue, myquery;
+//,5 , "day", 2021-07-22, 2021-07-22, myquery
+  //rest.stocks.previousClose("C:AAPL", ).then((value) => { myvalue = value;}).catch(console.log("Error!\n"));
+  stocksWS.onmessage = ({data}) => {
+    const [message] = JSON.parse(data);
+    stocksWS.send('{"action":"subscribe", "params":"AM.MSFT,A.MSFT"}');
+
+    switch(message.ev){
+      case "AM":
+        
+        break;
+      case "A":
+        break;
+      
+    }
+  }
+  stocksWS.send({action:"subscribe", params: "T.MSFT"});
   return (
     <>
       <main>
         <h2>StockCheck</h2>
+          {/* socket.addEventListener('open', function(event) {
+            socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
+          }); */}
+          
         <p>
-          The current price of AAPL is: 
+          The current price of AAPL is: {myvalue}
         </p>
       
       </main>
