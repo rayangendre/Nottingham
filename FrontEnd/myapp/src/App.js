9c0e389b6640af7d3a11fe042280dcc1fad82633
Trevor Bryant <tbryant.tbryant@gmail.com>
2022-03-09 20:15:51 -0800

    Stock price lookup working

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -3,11 +3,12 @@ import { Routes, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button"
 import './App.css';
+import axios from "axios"
 //import '../node_modules/react-vis/dist/style.css';
 //import {XYPlot, LineSeries} from 'react-vis';
 import { polygonClient, restClient, websocketClient } from "@polygon.io/client-js";
-const rest = restClient("nKHbbu_36PUwzwk9Ed90ViqDpxk4GFHH");
-const stocksWS = websocketClient("nKHbbu_36PUwzwk9Ed90ViqDpxk4GFHH").stocks();
+import { useState } from 'react';
+const apiKey = "Yhaw6WexncpW6UEMOiwDTI5s5zlVEFQa"
 
 function App() {
   return (
@@ -119,32 +120,35 @@ function LogIn() {
 
 function StockCheck(){
   let myvalue, myquery;
-//,5 , "day", 2021-07-22, 2021-07-22, myquery
-  //rest.stocks.previousClose("C:AAPL", ).then((value) => { myvalue = value;}).catch(console.log("Error!\n"));
-  stocksWS.onmessage = ({data}) => {
-    const [message] = JSON.parse(data);
-    stocksWS.send('{"action":"subscribe", "params":"AM.MSFT,A.MSFT"}');
+  const [price, setPrice] = useState(null)
+  const [ticker, setTicker] = useState("")
 
-    switch(message.ev){
-      case "AM":
-        
-        break;
-      case "A":
-        break;
-      
-    }
+  async function HandleSubmit(e){
+    console.log(e.target.ticker.value)
+    e.preventDefault()
+    const stockPrice = await checkPrice(e.target.ticker.value)
+    console.log(stockPrice)
+    setTicker(e.target.ticker.value)
+    setPrice(stockPrice)
   }
-  stocksWS.send({action:"subscribe", params: "T.MSFT"});
+
+  async function checkPrice(ticker){
+    return await axios.get("https://api.polygon.io/v2/aggs/ticker/".concat(ticker).concat("/prev?adjusted=true&apiKey=").concat(apiKey))
+  }
+
   return (
     <>
       <main>
         <h2>StockCheck</h2>
-          {/* socket.addEventListener('open', function(event) {
-            socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
-          }); */}
-          
+        <form onSubmit={HandleSubmit}>
+          <div class="mb-3">
+            <label class="form-label">Ticker</label>
+            <input name="ticker" type="text" class="form-control"/>
+            <button type="submit" class="btn btn-outline-primary" >Check Price</button>
+          </div>
+        </form>
         <p>
-          The current price of AAPL is: {myvalue}
+          The current price of {ticker} is {price ? price.data.results[0].c : ""}: 
         </p>
       
       </main>
