2b0d276ef06d049f39731f58724b863c298d5a50
rayangendre <64104432+rayangendre@users.noreply.github.com>
2022-06-01 23:17:36 +0000

    Deploying to gh-pages from @ rayangendre/Nottingham@1369346837da81d6b941e8a9848ef31184c18a10 🚀

---
+++ precache-manifest.d4050f347c044ab9556243f794507532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32a853e696f676c096f",
+    "url": "/Nottingham/myapp/static/css/main.56996be3.chunk.css"
+  },
+  {
+    "revision": "c32a853e696f676c096f",
+    "url": "/Nottingham/myapp/static/js/main.c32a853e.chunk.js"
+  },
+  {
+    "revision": "e94d872e0cc5161a7576",
+    "url": "/Nottingham/myapp/static/js/1.e94d872e.chunk.js"
+  },
+  {
+    "revision": "095f81e3501df31f2650",
+    "url": "/Nottingham/myapp/static/css/2.b3390e09.chunk.css"
+  },
+  {
+    "revision": "095f81e3501df31f2650",
+    "url": "/Nottingham/myapp/static/js/2.095f81e3.chunk.js"
+  },
+  {
+    "revision": "51aa2b1787506494b9dc",
+    "url": "/Nottingham/myapp/static/js/runtime~main.51aa2b17.js"
+  },
+  {
+    "revision": "479947ccc6f2137a0ac848c9f25eb957",
+    "url": "/Nottingham/myapp/index.html"
+  }
+];
