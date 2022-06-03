12c6b0a415b5bb79732d200f56c38612cb9b5656
rayangendre <rayan.gendre@gmail.com>
2022-06-02 19:35:43 -0700

    homepage

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -11,10 +11,32 @@ import React from "react";
 require("dotenv").config();
 
 function App() {
-  const ticker = "SPY";
+  const tickers = [
+    "SPY",
+    "AAPL",
+    "TSLA",
+    "GOOGL",
+    "NKE",
+    "AMZN",
+    "QQQ",
+    "CRM",
+    "SHOP",
+    "MSFT",
+    "PYPL",
+    "TTD",
+    "TWLO",
+  ];
+  const ticker = tickers[Math.floor(Math.random() * 6)];
   return (
-    <div>
+    <div class="center">
       <Navbar />
+      <h2>WELCOME TO NOTTINGHAM</h2>
+      <p1 class="lower-header">
+        We are the leaders in stock trading here at Nottingham
+      </p1>
+      <p>Let us help you reach financial freedom</p>
+      <h4>Featured Stock</h4>
+      <h2>{ticker}</h2>
       <StockChart symbol={ticker}></StockChart>
     </div>
   );
